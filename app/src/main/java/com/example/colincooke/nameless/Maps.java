cf3c5eabae0893d5f0a3085e1941276211ab7982
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-07-09 13:31:01 -0700

    build maps class

--- app/src/main/java/com/example/colincooke/nameless/Maps.java
+++ app/src/main/java/com/example/colincooke/nameless/Maps.java
@@ -1,7 +1,35 @@
 package com.example.colincooke.nameless;
 
+import org.json.JSONObject;
+import android.location.Location;
+
 /**
  * Created by colincooke on 7/9/16.
  */
 public class Maps {
+
+    Location location;
+    double zoom = 10;
+
+    public Maps(double z) {
+        zoom = z;
+    }
+
+    public void requestMap() {
+        location = LocationUtils.getCurrentLocation();
+
+        JSONObject obj = new JSONObject();
+
+        try {
+            obj.put("type", "map");
+            obj.put("lon", Double.valueOf(location.getLongitude()));
+            obj.put("lat", Double.valueOf(location.getLatitude()));
+            obj.put("zoom", Double.valueOf(zoom));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        TextUtil.sendText("15714712696", obj.toString());
+    }
+
 }
